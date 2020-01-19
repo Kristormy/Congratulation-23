abfdf544f88feba1e7f0a37314a03f03ffbd0773
Кристина Кальницкая <59908443+Kristormy@users.noreply.github.com>
2020-01-19 12:10:28 +0300

    Update play.js

--- js/play.js
+++ js/play.js
@@ -1,11 +1,12 @@
+   var hit = 0;
   var miss = 0;
-    var hit = 0;
+    
         
     $(document).ready(function() {
 
         var play;
 
-        $("#btnstart").click(function(){
+        $("#start").click(function(){
             startplay();
         });
 
@@ -14,15 +15,18 @@
             $(".character").fadeOut('slow');
             $("#hit").html("0 попал");
 			$("#miss").html("0 мимо");
-            hit = 0;
+           
+			hit = 0;
 			miss = 0;
-            $("#btnstart").css("color", "#e3e3e3");
-            $("#btnstart").unbind("click");
-            play = setInterval(scramble, 1800);
+            
+			$("#start").css("color", "#e3e3e3");
+            $("#start").unbind("click");
+            
+			play = setInterval(scramble, 2000);
             setTimeout(function() {
                 clearInterval(play);
-                $("#btnstart").css("color", "#333333");
-                $("#btnstart").bind("click", startplay);
+                $("#start").css("color", "#333333");
+                $("#start").bind("click", startplay);
 
                 // Выводим финальное сообщение
                 var containerPos = $('#container').offset();
@@ -31,27 +35,27 @@
                     left: containerPos.left
                     }, 'fast', function() {
                         setTimeout(function() {
-                            $("#message").fadeIn('slow');
-                            
+                            $("#message").fadeIn('slow');                           
                         }, 500);
                 });
-            }, 5000);
+            }, 15000);
         }
 
         $(".character").click(function() {
-            if ($(this).hasClass("ally")) {
+            if ($(this).hasClass("animals")) {
                 $(this).effect("bounce", {
-                    times: 2,
+                    times: 3,
                     direction: 'left'
                 }, 300);
                 $(this).slideUp("fast");
                 miss++;
                 $("#miss").html(miss + " мимо");
-            } else {
+            } 
+			else {
 
                 $(this).effect("explode", 500);
                 hit++;
-                $("#hit").html(hit + " в цель");
+                $("#hit").html(hit + " попал");
             }
         });
     });
@@ -65,9 +69,10 @@
 
         var randomId = randomFromTo(1, children.length);
         moveRandom('char'+randomId);
-        setTimeout(function(){
+        
+		setTimeout(function(){
             $("#char"+randomId).slideDown('fast');            
-        }, 500);
+        }, 100);
         
         setTimeout(function() {
             $("#char"+randomId).slideUp('fast');
@@ -76,25 +81,25 @@
 
     function moveRandom(id) {
         /* Получаем положение контейнера и размер
-         * -- метод доступа: cPos.top и cPos.left */
-        var cPos = $('#container').offset();
-        var cHeight = $('#container').height();
-        var cWidth = $('#container').width();
+         * -- метод доступа: SizePos.top и SizePos.left */
+        var SizePos = $('#container').offset();
+        var SizeHeight = $('#container').height();
+        var SizeWidth = $('#container').width();
 
         // Получаем отступы прямоугольника (предполагаем, что все отступы равны)
         var pad = parseInt($('#container').css('padding-top').replace('%', ''));
 
         // Получаем размер перемещаемого прямоугольника
-        var bHeight = $('#'+id).height();
-        var bWidth = $('#'+id).width();
+        var newHeight = $('#'+id).height();
+        var newWidth = $('#'+id).width();
 
         // Устанавливаем максимум для положения
-        maxY = cPos.top + cHeight - bHeight - pad;
-        maxX = cPos.left + cWidth - bWidth - pad;
+        maxY = SizePos.top + SizeHeight - newHeight - pad;
+        maxX = SizePos.left + SizeWidth - newWidth - pad;
 
         // Устанавливаем минимум для положение
-        minY = cPos.top + pad;
-        minX = cPos.left + pad;
+        minY = SizePos.top + pad;
+        minX = SizePos.left + pad;
 
         // Устанавливаем новое положение
         newY = randomFromTo(minY, maxY);
